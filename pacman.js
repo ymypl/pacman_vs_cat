9c1e4ee33d02a124836de84996b8c16ccad30c7c
Jeremiasz Pelczar <jeremiasz.pelczar@gmail.com>
2017-11-05 18:32:47 +0100

    Added cat

--- pacman.js
+++ pacman.js
@@ -9,6 +9,18 @@ class Pacman {
   up() {
     this.y -= STEP;
     this.update();
+
+  }
+
+  left() {
+    this.x -= STEP;
+    this.update();
+  }
+
+  right() {
+    this.x += STEP;
+
+    this.update();
   }
 
   down() {
@@ -35,6 +47,42 @@ class Pacman {
 
 }
 
+class Cat {
+  
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.update();
+  }
+
+  up() {
+    this.y -= STEP;
+    this.update();
+  }
+
+  left() {
+    this.x -= STEP;
+    this.update();
+  }
+
+  right() {
+    this.x += STEP;
+    this.update();
+  }
+
+  down() {
+    this.y += STEP;
+    this.update();
+  }
+
+  update() {
+    let $cat = $('.cat');
+    $cat.css("left", this.x + "px");
+    $cat.css("top", this.y + "px");
+  }
+
+}
+
 let schedule = (fn) => {
   window.setTimeout(() => {
     fn.apply();
@@ -56,11 +104,31 @@ let handleKey = (e) => {
     case 'ArrowRight':
       pacman.right();
       break;
+    case 'ArrowLeft':
+      pacman.left();
+      break;
+    case 'ArrowRight':
+      pacman.right();
+      break;
+    case 'w':
+      cat.up();
+      break;
+    case 'a':
+      cat.left();
+      break;
+      case 'd':
+      cat.right();
+      break;
+
+      case 's':
+      cat.down();
+      break;
   }
 }
 
 const STEP = 10;
 let pacman = new Pacman(100, 100);
+let cat = new Cat(300, 100);
 
 $(document).keydown(e => handleKey(e));
 
