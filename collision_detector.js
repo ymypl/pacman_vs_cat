a924094f3664f1ec45c364e7134481bfc06ba92e
mrderpi <patryk.pelczar@gmail.com>
2017-11-26 16:31:03 +0100

    cat_svg

--- collision_detector.js
+++ collision_detector.js
@@ -8,6 +8,10 @@ class CollisionDetector {
 	}
 
 	isCollision(x, y, width) {
+		for (let i = 0; i < this.walls.length; i++) {
+			let wall = this.walls[i];
+			
+		}
 		let check = y > 100;
 		return check;
 	}
